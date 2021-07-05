f3c9e05998570307273af41109cb51171a3b389e
pineapple028 <74632408+pineapple028@users.noreply.github.com>
2021-07-05 22:54:22 +0800

    Create LeetCode_49.java

---
+++ Algorithms/LeetCode_49.java
@@ -0,0 +1,22 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        
+        List<List<String>> result = new ArrayList<>();
+        
+        HashMap<String , List<String>> map = new HashMap<>();
+        
+        for(String current:strs){
+            char [] character=current.toCharArray();
+            Arrays.sort(character);
+            String sorted=new String(character);
+            if(!map.containsKey(sorted)){
+                map.put(sorted,new ArrayList<>());
+            }
+            
+            map.get(sorted).add(current);
+        }
+        
+        result.addAll(map.values());
+        return result;
+    }
+}
