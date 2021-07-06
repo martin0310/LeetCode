93336890e1738a44abc4c4e0785f5720bcf805f6
pineapple028 <74632408+pineapple028@users.noreply.github.com>
2021-07-06 14:24:11 +0800

    Create LeetCode_115.java

---
+++ Algorithms/LeetCode_115.java
@@ -0,0 +1,21 @@
+class Solution {
+    
+    public int count = 0;
+    
+    public int numDistinct(String s, String t) {
+        int [][] dp = new int [t.length() + 1][s.length() + 1]; 
+        for(int i = 0; i < dp[0].length; i++){
+            dp[0][i] = 1;
+        }
+        for(int i = 1; i < dp.length; i++){
+            for(int j = 1; j < dp[i].length; j++){
+                if(t.charAt(i - 1) == s.charAt(j - 1))
+                    dp[i][j] = dp[i -1][j - 1] + dp[i][j - 1];
+                else
+                    dp[i][j] = dp[i][j - 1];
+            }
+        }
+        
+        return dp[t.length()][s.length()];
+    }
+}
