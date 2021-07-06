74cc780357e0d37f6f8421f920850c6b37da9324
pineapple028 <74632408+pineapple028@users.noreply.github.com>
2021-07-06 14:01:17 +0800

    Create LeetCode_88.java

---
+++ Algorithms/LeetCode_88.java
@@ -0,0 +1,11 @@
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        for(int i=m;i<nums1.length;i++){
+            nums1[i]=nums2[n-1];
+            n--;
+                if(n<0)
+                    break;
+        }
+        Arrays.sort(nums1);
+    }
+}
